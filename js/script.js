73094a6abc9a00ffda70769fdf39438f06ea21bc
danpuweb <puinsdan@gmail.com>
2020-04-22 21:06:23 -0500

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function EstaPáginaTratasobre(){
+    alert("Enfermedades Psicológicas");
+}
+
+function nombreSitioweb(){
+    var nombresitio = prompt("cómo se llama este sitio web");
+
+    if (nombresitio == "Enfermedades Psicológicas"){
+        alert("El nombre es correcto");        
+
+    } else{
+        alert("El nombre es incorrecto");
+
+    }
+}
+
+function confirmación() {
+    return confirmación("quieres ir a la red social");
+}
